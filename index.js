90c85db380fc50a9fe45eb5adf0dfa8691612109
Volantis <putzstuck@gmail.com>
2022-11-03 14:01:38 -0400

    linked menu.js to index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inquirer = require('inquirer');
 const express = require('express');
 const sql = require('mysql2');
 
+const mainMenu = require('./lib/menu')
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,14 +16,14 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: 'root',
-    // MySQL password
     password: 'r32r33r34@GTR',
     database: 'employeeCMS_db'
   },
   console.log('Connected to the employeeCMS_db database.')
 );
+
+mainMenu(mainMenu);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}...`);
