25dfe97e14d045c32e4b8aa6e0424a9a7ac86558
Volantis <putzstuck@gmail.com>
2022-11-02 20:12:29 -0400

    corrected issues with schema file to create database and tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE employeeCMS_db;
 USE employeeCMS_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT(10,2) PRIMARY KEY,
+ 	id int AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT(10,3) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
@@ -20,8 +20,8 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT AUTO_INCREMENT(1000,127),
-    manager_id INT AUTO_INCREMENT(100,12) REFERENCES (id),
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
 );
